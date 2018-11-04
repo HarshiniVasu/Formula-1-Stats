d34011f923143c4e898a527d2f7f894e18a95482
Sanjana A <sanjana.aravindana95@gmail.com>
2018-11-04 15:21:37 -0700

    initial check

--- code/js/script.js
+++ code/js/script.js
@@ -1,6 +1,6 @@
 console.log("hello");
 d3.csv("/data/final_f1_stats.csv").then( matchesCSV => {
-	matchesCSV.forEach(d,i){
+	matchesCSV.forEach((d,i) => {
 		console.log(d.country);
-	}
+		});
 	});
