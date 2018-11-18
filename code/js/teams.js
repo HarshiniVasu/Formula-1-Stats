fd145deef04afdfd84a481c4f2bbd05ce034dfb4
Vinu Sreenivasan <vinu.sreenivasan92@gmail.com>
2018-11-17 20:28:08 -0700

    slider changes

--- code/js/teams.js
+++ code/js/teams.js
@@ -1,6 +1,5 @@
 class Teams {
     constructor () {
-        this.activeYear = 1990;
         this.drawYearBar();
     }
 
@@ -12,34 +11,10 @@ class Teams {
 
         let that = this;
 
-        //Slider to change the activeYear of the data
-        let yearScale = d3.scaleLinear().domain([1950, 2018]).range([30, 730]);
-
-        let yearSlider = d3.select('#year-slider')
-            .append('div').classed('slider-wrap', true)
-            .append('input').classed('slider', true)
-            .attr('type', 'range')
-            .attr('min', 1950)
-            .attr('max', 2018)
-            .attr('value', this.activeYear);
-
-        let sliderLabel = d3.select('.slider-wrap')
-            .append('div').classed('slider-label', true)
-            .append('svg');
-
-        let sliderText = sliderLabel.append('text').text(this.activeYear);
-
-        sliderText.attr('x', yearScale(this.activeYear));
-        sliderText.attr('y', 25);
-
-        yearSlider.on('input', function() {
-            that.activeYear = this.value;
-            sliderText.text(this.value);
-            sliderText.attr('x', yearScale(this.value));
+        let slider = createD3RangeSlider(1950, 2018, "#slider-container");
+        slider.onChange(function(newRange){
+            d3.select("#range-label").text(newRange.begin + " - " + newRange.end);
         });
-
-        yearSlider.on('click', function() {
-            d3.event.stopPropagation();
-        });
+        slider.range(1970,1980);
     }
 }
